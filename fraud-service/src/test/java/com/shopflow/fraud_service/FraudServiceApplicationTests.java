d4260b94059c3c2695fcb29754b957431fab65f7
Bhuvan chandra <doniparthibhuvan_chandra@epam.com>
2026-03-12 22:15:35 +0530

    feat: create microservice modules and parent pom

---
+++ fraud-service/src/test/java/com/shopflow/fraud_service/FraudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopflow.fraud_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
